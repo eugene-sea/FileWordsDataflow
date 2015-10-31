0dbedc18797124795384f823448f98efb22930a8
Eugene Shalyuk <eugene.sea@gmail.com>
2015-10-31 21:43:23 +0200

    Added test for files enumerator block

---
+++ FileWordsDataflow.Services/DataflowBlocks/FilesEnumerator.cs
@@ -0,0 +1,12 @@
+﻿namespace FileWordsDataflow.Services.DataflowBlocks
+{
+    using System.Threading.Tasks.Dataflow;
+
+    internal static class FilesEnumerator
+    {
+        public static ISourceBlock<string> GetFilesEnumeratorBlock(string path, string searchPattern)
+        {
+            return null;
+        }
+    }
+}
---
+++ FileWordsDataflow.Tests/FilesEnumeratorTests.cs
@@ -0,0 +1,26 @@
+﻿namespace FileWordsDataflow.Tests
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Threading.Tasks.Dataflow;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Services.DataflowBlocks;
+
+    [TestClass]
+    public class FilesEnumeratorTests
+    {
+        [TestMethod]
+        public void ShouldEnumerateTextFilesInAllSubfolders()
+        {
+            var block = FilesEnumerator.GetFilesEnumeratorBlock("TestFolder", "*.txt");
+            var files = Enumerable.Repeat(1, 3).Select(_ => block.Receive(TimeSpan.FromMilliseconds(100))).ToList();
+            block.Completion.Wait();
+            Assert.IsTrue(
+                files
+                    .OrderBy(f => f)
+                    .SequenceEqual(Enumerable.Range(1, 3)
+                    .Select(i => string.Format(CultureInfo.InvariantCulture, "TextFile{0}.txt", i))));
+        }
+    }
+}
