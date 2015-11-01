e58cde76c44c66ce8cadfbbe78e9664887f4e23f
Eugene Shalyuk <eugene.sea@gmail.com>
2015-11-01 22:47:40 +0200

    Added file saver block

---
+++ FileWordsDataflow.Dal/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace FileWordsDataflow.Dal
+{
+    using System.Collections.Generic;
+    using DataModel;
+
+    public interface IRepository
+    {
+        void SaveFiles(IEnumerable<File> files);
+    }
+}
---
+++ FileWordsDataflow.Services/DataflowBlocks/FileSaver.cs
@@ -0,0 +1,25 @@
+﻿namespace FileWordsDataflow.Services.DataflowBlocks
+{
+    using System;
+    using System.Threading.Tasks.Dataflow;
+    using Dal;
+    using DataModel;
+
+    internal static class FileSaver
+    {
+        public static IPropagatorBlock<File, File> GetFileSaverBlock(Func<IRepository> repositoryFactory)
+        {
+            var batchBlock = new BatchBlock<File>(100);
+            var transformBlock = new TransformManyBlock<File[], File>(
+                files =>
+                {
+                    var repo = repositoryFactory();
+                    repo.SaveFiles(files);
+                    return files;
+                });
+            batchBlock.LinkTo(transformBlock);
+            batchBlock.Completion.ContinueWith(t => Utils.PropagateCompleted(t, transformBlock));
+            return DataflowBlock.Encapsulate(batchBlock, transformBlock);
+        }
+    }
+}
