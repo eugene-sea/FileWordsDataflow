5a2ecafa147b8463a44f595cad6a48573ca2c670
Eugene Shalyuk <eugene.sea@gmail.com>
2015-11-02 20:26:24 +0200

    Added file word creator block

---
+++ FileWordsDataflow.Services/DataflowBlocks/FileWordCreator.cs
@@ -0,0 +1,25 @@
+﻿namespace FileWordsDataflow.Services.DataflowBlocks
+{
+    using System.Collections.Concurrent;
+    using System.Threading.Tasks.Dataflow;
+    using DataModel;
+
+    internal static class FileWordCreator
+    {
+        public static IPropagatorBlock<LineSplitter.FileLineWord, FileWord> GetFileWordCreatorBlock()
+        {
+            var wordCache = new ConcurrentDictionary<string, Word>();
+            var resultsBlock = new BufferBlock<FileWord>();
+            var actionBlock = new ActionBlock<LineSplitter.FileLineWord>(
+                w =>
+                {
+                   var word = wordCache.GetOrAdd(w.Word, term => new Word { Term = term });
+                    resultsBlock.Post(new FileWord { File = w.File, Word = word, Row = w.Row, Col = w.Col });
+                },
+                new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = DataflowBlockOptions.Unbounded });
+
+            actionBlock.PropagateCompleted(resultsBlock);
+            return DataflowBlock.Encapsulate(actionBlock, resultsBlock);
+        } 
+    }
+}
---
+++ FileWordsDataflow.Tests/FileWordCreatorTests.cs
@@ -0,0 +1,29 @@
+﻿namespace FileWordsDataflow.Tests
+{
+    using System.Threading.Tasks.Dataflow;
+    using DataModel;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Services.DataflowBlocks;
+
+    [TestClass]
+    public class FileWordCreatorTests
+    {
+        [TestMethod]
+        public void ShouldReturnTheSameWordInstanceForAllOccurrencesOfThisWord()
+        {
+            var block = FileWordCreator.GetFileWordCreatorBlock();
+            block.Post(new LineSplitter.FileLineWord(new File(), "TEST", 1, 1));
+            var file = new File();
+            block.Post(new LineSplitter.FileLineWord(file, "TEST", 3, 14));
+            var word1 = block.ReceiveWithTimeout();
+            var word2 = block.ReceiveWithTimeout();
+            block.Complete();
+            block.EnsureCompleted();
+            Assert.AreSame(word1.Word, word2.Word);
+            Assert.AreEqual("TEST", word2.Word.Term);
+            Assert.AreSame(file, word2.File);
+            Assert.AreEqual(3, word2.Row);
+            Assert.AreEqual(14, word2.Col);
+        }
+    }
+}
