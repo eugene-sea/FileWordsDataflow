cd8e9d906baf55f16f0ba202ba63d7967fe394eb
Eugene Shalyuk <eugene.sea@gmail.com>
2015-11-04 20:32:27 +0200

    Module init for services

---
+++ FileWordsDataflow.Services/ModuleInit.cs
@@ -0,0 +1,23 @@
+﻿namespace FileWordsDataflow.Services
+{
+    using System;
+    using Common;
+
+    public class ModuleInit : IModule
+    {
+        public void Initialize(ApplicationParameters parameters, IModuleRegistrar registrar)
+        {
+            if (parameters == null)
+            {
+                throw new ArgumentNullException("parameters");
+            }
+
+            if (registrar == null)
+            {
+                throw new ArgumentNullException("registrar");
+            }
+
+            registrar.RegisterType<IFileWordsService, FileWordsService>();
+        }
+    }
+}
